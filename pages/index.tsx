751b51c25be50af1fa9c70b7b229cbf3d13246fc
jachvin17 <168075124+jachvin17@users.noreply.github.com>
2025-09-10 16:48:35 +0000

    hello world

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      {/* ...existing code... */}
+    </main>
+  );
+};
+
+export default MainPage;
